d9c5f1143cef7cc3ef897d1bfe441e9ec1f5d987
Eric <ckf_mo@yahoo.com.hk>
2023-07-02 20:19:39 +0800

    加入專案檔案。

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        public UserController(ILogger<UserController> logger) {
+            _logger = logger;
+        }
+        [HttpGet]
+        public String get()
+        {
+            _logger.LogInformation("My Log here.");
+            return "Get methed here...";
+        }
+    }
+}
